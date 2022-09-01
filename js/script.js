a5487d1c774a32fa437dea92ac5bdebcc66fad25
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-09-01 12:31:57 +0300

    Adds meals to DOM

--- js/script.js
+++ js/script.js
@@ -34,4 +34,82 @@ const addMeals=function(mealDataArr){
             });
     mealsEl.innerHTML=innerHTML.join('');
         };
-    
+    //fetch meals from the API
+    const searchMeal=async function(e){
+        e.preventDefault();
+        if(searchBox.value===''){
+            alert('Please enter a search term');
+            return;
+        }
+        const term=searchBox.value;
+        const mealsArr=[];
+        try{
+            //search meal by name
+            const mealData=await getJSON(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`);
+            if(mealData.meal!==null){
+                mealsArr.push(mealData);
+            }
+            //search by ingredient
+            const mealData2 = await getJSON(
+                `https://www.themealdb.com/api/json/v1/1/filter.php?i=${term}`
+              );
+              if (mealData2.meals !== null) mealsArr.push(mealData2);
+              //search by category
+              const mealData3 = await getJSON(
+                `https://www.themealdb.com/api/json/v1/1/filter.php?c=${term}`
+              );
+              if (mealData3.meals !== null) mealsArr.push(mealData3);
+              // API filter by area
+                const mealData4 = await getJSON(
+                    `https://www.themealdb.com/api/json/v1/1/filter.php?a=${term}`
+                );
+                if (mealData4.meals !== null) mealsArr.push(mealData4);
+            
+                resultHeading.innerHTML = `<h2> Search results for '${term}'</h2>`;
+                if (mealsArr.length === 0) {
+                    resultHeading.innerHTML = `<p> No search results for '${term}'. Please try again!</p>`;
+                    mealsEl.innerHTML = '';
+                    if (term === 'desert')
+                    resultHeading.innerHTML = `<p> No search results for '${term}'. Try "dessert"!`;
+                    return;
+                }else{
+                    //add meal from DOM
+                    displayMeals(mealsArr);
+                }
+                searchBox.value='';
+            }catch(err){
+                alert(err.message);
+            }
+        };
+
+        //work with api format
+        class recipeItem{
+            constructor(ingredients, quantity){
+                this.ingredient=this.ingredient;
+                this.quantity=quantity;
+
+            }
+        }
+    // add selected meal to DOM
+    const addMealToDOM=function(meal){
+        const ingredient=[];
+        const measurements=[];
+        const recipeData=[];
+
+        for (let key in meal) {
+            let word = key;
+            if (word.includes('strIngredient') && meal[key] !== '') {
+              ingredients.push(meal[key]);
+            }
+            if (word.includes('strMeasure') && meal[key] !== '') {
+              measurements.push(meal[key]);
+            }
+          }
+          for (let i = 0; i < ingredients.length; i++) {
+            let j = new recipeItem(ingredients[i], measurements[i]);
+            recipeData.push(j);
+          }
+
+          //add meal from dom
+
+    }
