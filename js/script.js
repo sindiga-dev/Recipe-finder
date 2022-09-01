2cba43296a841e932726fd5942f8ce592a88fdf9
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-09-01 11:54:40 +0300

    Adds JSON

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ const randomBtn=document.getElementById('random');
 const singleMealEL=document.getElementById('single-meal');
 const resultHeading=document.getElementById('result-heading');
 const mealsEl=document.getElementById('meals');
+const getJSON=async function(url ,errorMsg='Something went wrong!'){
+    return await fetch(url).then(res=>{
+        if(!res.ok){
+            resultHeading.innerHTML='<h2>There has been an error!</h2>';
+            throw new Error(`${errorMsg}(${res.status})`);
+        }
+        return res.json();
+    });
+}
