df66b393a3d16adcaa0d53c8318aebbdda80d2e3
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-09-01 11:45:10 +0300

    Creates variables

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+const searchBox=document.getElementById('search');
+const submitBtn=document.getElementById('submit');
+const randomBtn=document.getElementById('random');
+const singleMealEL=document.getElementById('single-meal');
+const resultHeading=document.getElementById('result-heading');
+const mealsEl=document.getElementById('meals');
